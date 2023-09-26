4ceb67e3cb70adf13309ebb40cd45f2648f6ef67
e1b20069 <e1b20069@st.oit.ac.jp>
2023-09-26 14:21:27 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1661/kaizi/janken/JankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1661.kaizi.janken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenApplication.class, args);
+	}
+
+}
